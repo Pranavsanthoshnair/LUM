f053b92b51ae0ea8e8b1756722f11f125746330a
Thomas Cherian <thomascherian2028@cs.sjcetpalai.ac.in>
2025-03-16 14:37:44 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -167,7 +167,7 @@ const Home = () => {
             <div className="group relative bg-white rounded-lg shadow overflow-hidden flex flex-col">
               <div className="relative card-image-container">
                 <img
-                  src="https://images.unsplash.com/photo-1559223607-a43f990c095d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                  src="https://unsplash.com/photos/person-holding-pencil-near-laptop-computer-5fNmWej4tAA"
                   alt="Corporate Brand Campaign"
                   className="group-hover:opacity-75 transition-opacity"
                 />
